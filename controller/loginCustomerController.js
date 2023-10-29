d3084f95f432cac6b2cef0e8f13a51937f89ff1e
mohd musayyib <mohd.musayyib@ksolves.com>
2023-10-29 23:11:59 +0530

    create jwt access and refresh token

--- controller/loginCustomerController.js
+++ controller/loginCustomerController.js
@@ -1,26 +1,43 @@
 const customer = require('../userModel/customerModel');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
 
 
 const customerLogin = async (req, res) => {
 
     const { email, password } = req.body;
-    if(!email || !password) return res.status(400).json({ 'message': 'email and password are required !' });
-   
+    if (!email || !password) return res.status(400).json({ 'message': 'email and password are required !' });
+
     const checkCustomer = await customer.findOne({ email }).exec();
-    if(!checkCustomer) return res.status(404).json({ 'message': 'customer is not present,Please register before the login' });
+    if (!checkCustomer) return res.status(404).json({ 'message': 'customer is not present,Please register before the login' });
 
     const checkpass = await bcrypt.compare(password, checkCustomer.password);
-     console.log(checkpass);
-    if(checkpass) {
-         res.status(200).json({' customer' : `${checkCustomer.customerName} Login successfully ` })
+    console.log(checkpass);
+    if (checkpass) {
+
+        const accessToken = jwt.sign(
+            { "email": checkCustomer.email },
+            process.env.ACCESS_TOKEN_SECRET,
+            { expiresIn: '40s' }
+        );
+        const refreshToken = jwt.sign(
+            { "email": checkCustomer.email },
+            process.env.ACCESS_TOKEN_SECRET,
+            { expiresIn: '1d' }
+        );
+
+        res.cookie('jwt', refreshToken, { httpOnly: true, maxAge: 24 * 60 * 60 * 1000 });
+        res.json({ accessToken });
+        // res.status(200).json({' customer' : `${checkCustomer.customerName} Login successfully ` })
     }
     else {
         res.status(401).json({ 'message': 'password not matched !' });
     }
 
 
-    
+
 }
 
 
--- userApi/user.js
+++ userApi/user.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controller/userController');
-
+const verifyJwt = require('../jwtMiddleware/jwtverify');
 
 
 router.route('/')
-    .get(userController.getAllUsers)
+    .get(verifyJwt, userController.getAllUsers)
     .post(userController.addUser)
     .put(userController.updateUser)
     .delete(userController.deleteUser);
 
 
-   module.exports = router; 
+module.exports = router; 
